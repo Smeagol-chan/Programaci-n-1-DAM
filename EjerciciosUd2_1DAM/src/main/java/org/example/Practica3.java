b96d962656c3d90b05a0fb768f54c27055a8ee3d
DV-5\ericr <ericrp188@gmail.com>
2025-10-25 03:52:11 +0200

    El case 2 funciona mal

--- EjerciciosUd2_1DAM/src/main/java/org/example/Practica3.java
+++ EjerciciosUd2_1DAM/src/main/java/org/example/Practica3.java
@@ -11,19 +11,19 @@ public class Practica3
         Scanner key = new Scanner(System.in);
         final int DIGITOS = 10; //Cantidad de dígitos que debe de conener un ISBN
         String isbn;    //Variable para guardar el número del usuario. También se encarga de comprobar si el usuario querrá reiniciar el programa o no.
-        int num = 0;    //Variable para hacer los cálculos. También es la variable encargada de seleccionar la opción del switch menú.
-        boolean fallo, vuelta = true;  //Controlar los bucles y reiniciar el programa.
+        int num = 0, aux, contadorInterrogante;    //Selector de switch. Variable para hacer los cálculos. contadorInterrogante aumenta en uno por cada ? encontrado en el isbn.
+        boolean fallo, vuelta = false;  //Controlar los bucles y reiniciar el programa.
 
         //do_while para poder reiniciar el programa si se desea
         do
         {
-            System.out.println("\t**** VALIDADOR Y REPARADOR DE CÓDIGOS ISBN ****\n");
+            System.out.println("\n\t**** VALIDADOR Y REPARADOR DE CÓDIGOS ISBN ****\n");
             //Pedir al usuario seleccionar una de las dos opciones que tiene el programa, solicitándolo de nuevo si no es válido.
             do
             {
                 fallo = false;
-                System.out.println("---------------------------\nSeleccione una opción:\n1. Validar\n2. Reparar\n---------------------------\n> ");
-                if(key.hasNextLine())
+                System.out.print("---------------------------\nSeleccione una opción:\n1. Validar\n2. Reparar\n\n> ");
+                if(key.hasNextInt())
                 {
                     num = key.nextInt();
                     if(num > 2 || num < 1)  //Me gusta comprobar si la opción es válida en este punto. El default del switch me gusta dejarlo libre por, si hay algún error y no puedo entrar en otra opción, saberlo.
@@ -37,6 +37,7 @@ public class Practica3
                     System.out.println("ERROR\nSe esperaba un entero.\n");
                     fallo = true;
                 }
+                key.nextLine();
             }while(fallo);
             System.out.println("---------------------------");
 
@@ -48,33 +49,137 @@ public class Practica3
                 fallo = false;
                 System.out.print("Introduzca el ISBN:\n> ");
                 isbn = key.nextLine();
+                isbn = isbn.toUpperCase();
+                contadorInterrogante = 0;   //Inicializo lso contadores para tenerlos a cero para cada nuevo intendo en los for.
 
+                //La primera comprobación es para la cantidad de dígitos introducidos. Tienen que ser 10.
                 if(isbn.length() == DIGITOS)
                 {
-                    
+                    //me he matado aquí.
+                    //Con un for compruebo si puedo volver el carácter en isbn en un entero. Si puedo, confirmo que es un número. Si no puedo, salta el catch y compruebo si es ? ó X. Cualquier otra cosa no es válida.
+                    for(int i = 0; i < DIGITOS; i++)
+                    {
+                        try
+                        {
+                            aux = Integer.parseInt(String.valueOf(isbn.charAt(i)));
+                        }
+                        catch (NumberFormatException e)
+                        {
+                            //Si no es un ? ó se trata de una X fuera de la última posición, fuerzo a contadorInterrogantes a ser negativa.
+                            contadorInterrogante += isbn.charAt(i) == '?' ? 1 : isbn.charAt(i) == 'X' ? i == 9 ? 0 : -100 : -100;
+                        }
+                    }
                 }
                 else
                 {
                     System.out.println("ERROR\nEl código ISBN debe contar con "+DIGITOS+" carácteres.\n");
                     fallo = true;
                 }
+
+                //Si se han encontrado fallo en el for anterior, contador es negativo.
+                if(contadorInterrogante < 0)
+                {
+                    System.out.println("ERROR\nCarácteres inválidos en el código.\n");
+                    fallo = true;
+                }
+                else if (contadorInterrogante > 1)  //Compruebo la cantidad de ? encontrados. Si supera 1 salta error.
+                {
+                    System.out.println("ERROR\n"+ contadorInterrogante +" interrogantes (?) introducidos. No se permiten más de 1 interrogante (?).\n");
+                    fallo = true;
+                }
             }while(fallo);
 
+            //switch donde se calcularán las operaciones que pide el usuario.
             switch (num)
             {
-                case 1:
-
+                case 1: //Primero compruebo que no se hayan introducido ?. Es un símbolo exclusivo del case 2.
+                    if(contadorInterrogante != 0)
+                    {
+                        System.out.println("ERROR\nNo se puede validar un código incompleto.\n");
+                        fallo = true;
+                    }
+                    else
+                    {
+                        //Limpio el contenido de num para poder usarlo en las sumas.
+                        num = 0;
+                        //un bucle for que multiplica las cifras por sus posiciones a la inversa +1. Si hay X se cambia por 10. Si el resultado es múltiplo de 11, se considera un ISBN válido.
+                        for(int i = DIGITOS; i > 0; i--)
+                        {
+                            aux = i == 1 ? isbn.contains("X") ? 10 : Integer.parseInt(String.valueOf(isbn.charAt(i-1))) : Integer.parseInt(String.valueOf(isbn.charAt(i-1)));
+                            num += i * aux;
+                        }
+                        System.out.println("El ISBN es "+ (num%11 == 0 ? "válido.\n": "inválido.\n"));
+                    }
                     break;
 
-                case 2:
+                case 2: //Primero hay que comprobar que tenga como mínimo un ?.
+                    if(contadorInterrogante != 1)
+                    {
+                        System.out.println("ERROR\nDebe haber 1 número perdido para poder repararlo.\n");
+                        fallo = true;
+                    }
+                    else
+                    {
+                        num = 0;
+                        //Usando el .indexOf encontramos dónde está situado el interrogante. Usamos el mísmo método que en el case 1, pero evitamos que se calcule el producto de la cifra que no conocemos.
+                        contadorInterrogante = isbn.indexOf("?");
+                        for(int i = DIGITOS; i > 0; i--)
+                        {
+                            if(isbn.charAt(contadorInterrogante) != '?')
+                            {
+                                aux = i == 1 ? isbn.contains("X") ? 10 : Integer.parseInt(String.valueOf(isbn.charAt(i-1))) : Integer.parseInt(String.valueOf(isbn.charAt(i-1)));
+                                num += i * aux;
+                            }
+                        }
+                        //Una vez teniendo todo calculado menos la cifra que no tenemos, nos metemos en un bucle while hasta encontrar el número que falta. While porque, si resulta que el que falta es 0, no hace falta entrar el bucle.
+                        aux = 0;
+                        while(((aux * (10 - contadorInterrogante)) + num)%11 != 0) aux++;   //10 - índice es importante, la posición es unversa al número por el que se multiplica. Luego multiplica a aux, suma y comprueba el resto en ese órden.
 
+                        System.out.println("El dígito que falta es "+ (contadorInterrogante == 9 ? aux == 10 ? "X" : aux : aux) +"\n");
+                    }
                     break;
 
                 default:
                     System.out.println("\n\t¡¡¡¡¡¡¡¡ESTÁS EN EL DEFAULT!!!!!!!!!!\n");
                     break;
             }
+
+            //bucle de regreso
+            if(!fallo)
+            {
+                do
+                {
+                    fallo = false;
+                    System.out.print("\n¿Desea reiniciar el programa? (si/no): ");
+                    isbn = key.nextLine();
+                    if(isbn.length() != 2)
+                    {
+                        System.out.println("ERROR\nFormato incorrecto. El texto ha de contar con 2 carácteres, usted ha introducido "+ isbn.length() +".");
+                        fallo = true;
+                    }
+                    else if (isbn.toLowerCase().contains("si"))
+                    {
+                            System.out.println();
+                            vuelta = true;
+                    }
+                    else if (!isbn.toLowerCase().contains("no"))
+                    {
+                        System.out.println("ERROR\nEl texto introducido no está permitido. Introduzca ó si ó no.\n");
+                        fallo = true;
+                    }
+                    else vuelta = false;
+
+                }while(fallo);
+            }
         }while(vuelta);
         System.out.println("\n\t**** FIN DE EJECUCIÓN ****");
     }
 }
+
+/*
+                            contadorX = contadorInterrogante = 0;
+                            (...)
+                            contadorInterrogante += i != 9 ? isbn.substring(i, i+1).contains(String.valueOf(j)) ? 0 : isbn.substring(i, i+1).contains("?") ? 1 : -100 : //Si i!=9 == true
+                                    isbn.substring(i).contains(String.valueOf(j)) ? 0 : isbn.substring(i).contains("?") ? 1 : isbn.substring(i).contains("X") ? contadorX++ : -100;    //Si i!=9 == false
+                                    No funciona, pero qué bonito que ha quedado
+                             */
--- EjerciciosUd2_1DAM/src/main/java/org/example/borrador.java
+++ EjerciciosUd2_1DAM/src/main/java/org/example/borrador.java
@@ -1,6 +1,7 @@
 package org.example;
 
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class borrador
@@ -8,6 +9,7 @@ public class borrador
     static void main()
     {
         Scanner key = new Scanner(System.in);
+        /*
         System.out.print("Número: ");
         int n = key.nextInt();
         //InputMismatchException comprende desde formatos erróneos hasta el StackOverflow
@@ -17,5 +19,60 @@ public class borrador
         String numero = key.nextLine();
         n = Integer.parseInt(numero);
         //NumberFormatException controla el .parseInt
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+         */
+
+//        String heces = "123";
+//        int num = 2;
+//
+//        System.out.println(num == Integer.parseInt(heces.substring(1, 2))); //Joder, no entiendo como Jesucristo es tan reverenciado teniéndome a mi.
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//        int num = 4;
+//        String culo = "pedo";
+//
+//        try
+//        {
+//            if(num == Integer.parseInt(culo.substring(0, 1))) System.out.println("AAAAAAAAAAA");
+//        }
+//        catch (NumberFormatException e)
+//        {
+//            System.out.println("CAGASTE "+ e.getMessage());
+//        }
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//        String code = "0123456789";
+//
+//        for(int i = 0; i < code.length(); i++) System.out.println( i!=code.length()-1 ? code.substring(i, i+1).contains(String.valueOf(0)) : code.substring(i).contains(String.valueOf(4)));
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//        int num = 6;
+//        String code = key.nextLine();
+//        try {
+//            num = num != 6 ? Integer.parseInt(code.substring(6-5, 6-4)) : Integer.parseInt(code.substring(6, 6+1));
+//            System.out.println(num);
+//        }catch (Exception e){
+//            System.out.println("porfa");
+//        }
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+        final int DIGITOS = 10;
+        int num, contadorInterrogante = 0;
+        String isbn = key.nextLine();
+
+        for(int i = 0; i < DIGITOS; i++)
+        {
+
+
+            try
+            {
+                num = Integer.parseInt(String.valueOf(isbn.charAt(i)));
+            }
+            catch (NumberFormatException e)
+            {
+                contadorInterrogante += isbn.charAt(i) == '?' ? 1 : isbn.charAt(i) == 'X' ? i == 9 ? 0 : -100 : -100;
+            }
+        }
+        System.out.println(contadorInterrogante);
+
+        System.out.println(isbn.indexOf("1"));
     }
 }
