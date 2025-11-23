ec289451e4aa25ae92037954bbd1088129fff065
DV-5\ericr <ericrp188@gmail.com>
2025-11-23 16:43:55 +0100

    Práctica 3 destruida sn breaks porque son feos y tontos y huelen a pis

--- EjerciciosUd3_1DAM/src/main/java/org/example/Practica3.java
+++ EjerciciosUd3_1DAM/src/main/java/org/example/Practica3.java
@@ -1,4 +1,5 @@
 package org.example;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Practica3
@@ -7,7 +8,8 @@ public class Practica3
     {
         Scanner key = new Scanner(System.in);
         final int RESOLUCION_MAX = 48, RESULUCION_MIN = 8;
-        int alto = 0, ancho = 0;
+        String fila, pixel1ro, pixel2do;
+        int alto = 0, ancho = 0, byteColumn = 0, byteRow, column, row;
         boolean fallo, segundoDato = false;
 
         System.out.println("\t**** VALIDADOR DE COMPATIBILIDAD ZX SPECTRUM ****\nIntroduce la resolución de tu pantalla (ancho x alto)...");
@@ -19,29 +21,115 @@ public class Practica3
             {
                 alto = key.nextInt();
             }
-            catch (NumberFormatException e)
+            catch (InputMismatchException e)
             {
                 System.out.println("ERROR\nSolo se permiten números.\n");
                 fallo = true;
+                key.nextLine();
             }
-            if(alto >= RESULUCION_MIN && alto <= RESOLUCION_MAX)
+            if(!fallo)
             {
-                if(!segundoDato) ancho = alto;
-                segundoDato = !segundoDato;
-            }
-            else
-            {
-                System.out.println("ERROR\nEl valor no puede ser inferior a "+ RESULUCION_MIN +" ni superior a "+ RESOLUCION_MAX +".\n");
-                fallo = true;
+                if(alto >= RESULUCION_MIN && alto <= RESOLUCION_MAX)
+                {
+                    if(alto%8 == 0)
+                    {
+                        if(!segundoDato) ancho = alto;
+                        segundoDato = !segundoDato;
+                    }
+                    else
+                    {
+                        System.out.println("ERROR\nLa resolución ha de ser un múltiplo de 8.\n");
+                        fallo = true;
+                    }
+                }
+                else
+                {
+                    System.out.println("ERROR\nEl valor no puede ser inferior a "+ RESULUCION_MIN +" ni superior a "+ RESOLUCION_MAX +".\n");
+                    fallo = true;
+                }
             }
         }while(fallo || segundoDato);
         String resolucion[][] = new String[alto][ancho];
+        key.nextLine();
 
         System.out.println("Introduce los colores de tu imagen píxel a píxel:");
         for(int i = 0; i < alto; i++)
         {
-            String fila[] = key.next().split("");
-            if(fila.length < )
+            System.out.print("> Fila "+ (i+1) +": ");
+            fila = key.nextLine();
+
+            if(fila.length() == ancho)
+            {
+                if(fila.matches("[A-O]+"))
+                {
+                    resolucion[i] = fila.split("");
+                }
+                else
+                {
+                    System.out.println("ERROR\nValor de píxel incorrecto (A-O).\n");
+                    i--;
+                }
+            }
+            else
+            {
+                System.out.println("ERROR\nSe deben introducir "+ ancho +" píxeles por fila.\n");
+                i--;
+            }
+        }
+
+        System.out.println("\n");
+        for(int i = 0; i < alto; i++)
+        {
+            for(int j = 0; j < ancho; j++)
+            {
+                System.out.print(resolucion[i][j] +" ");
+            }
+            System.out.println();
+        }
+
+        while(byteColumn < alto/8 && !fallo)
+        {
+            byteRow = 0;
+            while(byteRow < ancho/8 && !fallo)
+            {
+                pixel1ro = resolucion[8*byteColumn][8*byteRow];
+                pixel2do = "z";
+                column = (8*byteColumn);
+                while(column < (8*byteColumn)+8 && !fallo)
+                {
+                    row = (8*byteRow);
+                    while(row < ((8*byteRow)+8)-1 && !fallo)
+                    {
+                        if(!resolucion[column][row].contains(resolucion[column][row+1]))
+                        {
+                            if(!resolucion[column][row+1].contains(pixel1ro))
+                            {
+                                if(pixel2do.contains("z"))
+                                {
+                                    pixel2do = resolucion[column][row+1];
+                                }
+                                else
+                                {
+                                    if(!resolucion[column][row+1].contains(pixel2do)) fallo = true;
+                                }
+                            }
+                        }
+                        row++;
+                    }
+                    column++;
+                }
+                byteRow++;
+            }
+            byteColumn++;
+        }
+
+        if(!fallo)
+        {
+            System.out.println("\nEs compatible con un ZX Spectrum.");
+        }
+        else
+        {
+            System.out.println("\nNo es compatible con un ZX Spectrum.");
         }
     }
 }
