8466920f1efa7b075fd9502efb42aeadd052ac9b
Eric Ramos 1ºDAM <erirampas@alu.edu.gva.es>
2026-03-20 19:46:23 +0100

    Aplicar bono 3x2

--- EjerciciosUd7_1DAM/Mercadam/src/main/java/org/example/mercadam/Pedido.java
+++ EjerciciosUd7_1DAM/Mercadam/src/main/java/org/example/mercadam/Pedido.java
@@ -1,8 +1,5 @@
 package org.example.mercadam;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Pedido
 {
@@ -17,7 +14,7 @@ public class Pedido
 
     private void actualizarImporteTotal(double importe)
     {
-
+        importeTotal += importe;
     }
 
     public void actualizarPedido(Producto producto)
@@ -27,7 +24,19 @@ public class Pedido
 
     public void aplicarPromo3x2()
     {
-
+        int cantidadReducir;
+        for(Map.Entry<Producto, Integer> producto : pedido.entrySet())
+        {
+            if(producto.getValue() >= 3)
+            {
+                cantidadReducir = producto.getValue()/3;
+                for(int i = 0; i < cantidadReducir; i++)
+                {
+                    producto.setValue(producto.getValue()-1);
+                    actualizarImporteTotal(-1*producto.getKey().getPrecio());
+                }
+            }
+        }
     }
 
     public void aplicarPromo10()
